3fca7dc036e49be62cd59555f4d9e9425adade78
OstapMykhailiv <115294426+OstapMykhailiv@users.noreply.github.com>
2023-01-21 21:16:14 +0200

    Add files via upload

---
+++ deckofcards/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) {
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank);
+                System.out.println(card.rankToString(card.getRank()) + " " + card.suitToString(card.getSuit()));
+
+            }
+        }
+    }
+}
