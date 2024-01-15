7fb1e359959b3eaae70aceb012c759cce5e4bf38
ostapmykhailiv <115294426+OstapMykhailiv@users.noreply.github.com>
2024-01-15 14:20:54 +0200

    Add files via upload

--- CharSequence/src/Cs.java
+++ CharSequence/src/Cs.java
@@ -1,9 +1,14 @@
 import java.lang.CharSequence;
 
 public class Cs implements CharSequence {
-    private final String sentence = "The Java Tutorials";
+    private final String sentence = "Select one of the sentences from this book to use as the data.";
+    //Finds the index of last character
+    private int fromEnd(int i) {
+        return sentence.length() - 1 - i;
+    }
+
     //Finds the length
-@Override
+    @Override
     public int length() {
         int theLength = 0;
         for (int i = 0; i < sentence.length(); i++) {
@@ -11,45 +16,32 @@ public class Cs implements CharSequence {
         }
         return theLength;
     }
-    //Counts from the end
-    private int fromEnd(int i) {
-        return sentence.length() - 1 - i;
+
+    //Finds the character at needed index
+    @Override
+    public char charAt(int i) {
+        if ((i < 0) || (i >= sentence.length())){
+            throw new StringIndexOutOfBoundsException(i);
+        }
+        return sentence.charAt(fromEnd(i));
     }
-    //Returns a char on the i-position
-public char charAt(int i){
-   if ((i<0)||(i>sentence.length())){
-       throw new StringIndexOutOfBoundsException(i);
-   }
-   return sentence.charAt(sentence.length()-1-i);
-}
-    //Returns the sentence backwards
+    //Finds the subSequence that is needed to be reversed
     @Override
     public CharSequence subSequence(int start, int end) {
-        if (start < 0) {
+        if (start < 0){
             throw new StringIndexOutOfBoundsException(start);
         }
-        if (end > sentence.length()) {
+        if (end > sentence.length()){
             throw new StringIndexOutOfBoundsException(end);
         }
-        if (start > end) {
-            throw new StringIndexOutOfBoundsException(start - end);
+        if (start > end){
+            throw new StringIndexOutOfBoundsException(start);
         }
         StringBuilder sub = new StringBuilder(sentence.subSequence(fromEnd(end), fromEnd(start)));
         return sub.reverse();
     }
-    public String toString() {
-        StringBuilder sentence = new StringBuilder(this.sentence);
-        return sentence.reverse().toString();
-    }
     public static void main(String[] args) {
-    Cs myobj=new Cs();
-    System.out.println(myobj.subSequence(0, 17));
-    System.out.println(myobj.toString());
-    for (int i=0; i<myobj.length(); i++)
-    {
-        System.out.print(myobj.charAt(i));
+        Cs cs = new Cs();
+        System.out.println(cs.subSequence(0, cs.sentence.length()-1));
     }
-        System.out.println("");
-
-}
 }
